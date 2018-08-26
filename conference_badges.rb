45bc5e6d9a6116d1dcaff1a04232f01c379332bd
Scoti Dodson <scotidodson@gmail.com>
2018-08-26 04:51:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,11 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  
+def batch_badge_creator(attendees)
+  badge_list = []
+  attendes.each |a| do 
+    badge_maker(a)
+    >>
 end
 
 def assign_rooms
