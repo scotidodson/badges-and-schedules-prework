57169a9fdf7729f7ba7566b63242084ca201512a
Scoti Dodson <scotidodson@gmail.com>
2018-08-26 05:10:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,8 @@ end
 
 def assign_rooms(attendees)
   assigned_rooms = []
-  attendees.each_with_index do |name|
-    assigned_rooms << "Hello, #{name}! You'll be assigned to room #{number}!"
+  attendees.each_with_index do |name, index|
+    assigned_rooms << "Hello, #{name}! You'll be assigned to room " + (index + 1) + "!"
 end
 
 def printer
